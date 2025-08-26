78e5ac7d6402ed18f199e452ca10a8e0c93f66a6
Montgomery Mcdonald <halfbloodhero1027@gmail.com>
2025-08-26 15:26:17 -0700

    Super Epic Progress made the home and menu page, and the express code to launch the server (app.js) npm run dev to start nodemon on the ejs server.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+// middleware to serve static files (above)
+
+// middleware is a function that sits between the request from the client and the response from the server - think of it as a gatekeeper or pipeline every request and response goes through
+
+console.log(__dirname)
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.get('/contact.html',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'))
+})
+app.get('/locations.html',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'location.html'))
+})
+app.get('/menu.html',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'menu.html'))
+})
+app.get((req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
+app.listen(5000, ()=>{console.log('server is running on https://localhost:5000')})
